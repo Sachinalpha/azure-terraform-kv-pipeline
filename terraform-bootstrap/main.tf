11c80db67dc10f9796cc31841c6caf2319f4a584
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-11-24 16:17:28 +0530

    Add Azure Resource Group and Key Vault resources

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -1,1 +1,15 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${var.resource_group_name}-kv"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+}
 
