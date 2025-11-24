1a1f17670324973df7d5642dd4cbcc5a25692bfa
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-11-25 00:35:14 +0530

    Update main.tf

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -11,5 +11,6 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name         = azurerm_resource_group.rg.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
+  enable_rbac_authorization   = false  # Force Vault Access Policy mode
 }
 
