7ecd77f55d13a0058cc41d36f01c1de883eaf2c8
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2025-11-24 18:20:28 +0530

    Update main.tf

--- terraform-provisioner/main.tf
+++ terraform-provisioner/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "private" {
   address_prefixes     = ["10.0.1.0/24"]
 
   # Required for Private Endpoint
-  private_endpoint_network_policies_enabled = false
+  enforce_private_link_endpoint_network_policies = false
 }
 
 # 6️⃣ Key Vault Access Policy
