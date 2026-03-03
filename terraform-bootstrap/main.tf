bdab56834e252c1367e4071b7523301df9e1632f
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2026-03-03 13:18:06 +0530

    Update main.tf

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -1,16 +1,42 @@
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+# --------------------------------------------
+# Random number for uniqueness
+# --------------------------------------------
+resource "random_integer" "kv_rand" {
+  min = 100
+  max = 999
 }
 
-resource "azurerm_key_vault" "kv" {
-  name                        = "${var.resource_group_name}-kv"
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "premium"
-  enable_rbac_authorization   = false  # Force Vault Access Policy mode
+# --------------------------------------------
+# Key Vault Name 
+# --------------------------------------------
+locals {
+  segments         = split("-", var.rg_name)
+  n-name           = slice(local.segments, 0, 4)
+  trimmed_segments = [for s in local.n-name : substr(s, 0, 9)]
+  kv_base          = join("", local.trimmed_segments)
+
+  # Final Key Vault name
+  kv_final_name = lower("${local.kv_base}${random_integer.kv_rand.result}key")
+
+   # PEP name creation
+  rg_segments = split("-", var.rg_name)
+  pep_part1 = local.rg_segments[0]               
+  pep_part2 = local.rg_segments[1]               
+  pep_part3 = substr(local.rg_segments[2], 0, 9) 
+  pep_part4 = local.rg_segments[3]               
+  pep_number = random_integer.kv_rand.result  
+
+  kv_pep_name = lower("${local.pep_part1}-${local.pep_part2}-${local.pep_part3}-${local.pep_part4}-${local.pep_number}-pep")
+
 }
 
+module "kvault" {
+  source              = "../Modules/Key Vault"
+  key_vault_name      = local.kv_final_name
+  resource_group_name = var.rg_name
+  tenant_id           = var.tenant_id
+  vnet_name           = var.vnet_name
+  vnet_rg             = var.vnet_rg
+  shared_subnet_name    = var.subnet_name
+  private_endpoint_name = local.kv_pep_name
+}
