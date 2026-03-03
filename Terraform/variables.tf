04dee45d991a4b455c9a1e2238dd0fdf732eca0e
Sachinalpha <108449743+Sachinalpha@users.noreply.github.com>
2026-03-03 13:21:33 +0530

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,34 @@
+
+
+variable "tenant_id" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
+variable "client_id" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
+variable "client_secret" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
+variable "subscription_id" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
+variable "rg_name" {
+  description = "1 The name of the resource group where the key vault will be created"
+  type        = string
+}
+variable "vnet_name" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
+variable "vnet_rg" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
+variable "subnet_name" {
+  description = "The Tenand ID where the key vault will be deployed"
+  type        = string
+}
